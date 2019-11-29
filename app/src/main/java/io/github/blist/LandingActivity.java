967c8d17e2f6e8148808fe052b0d491ddee4059a
irene-haeryun-kwon <haeryun2004@gmail.com>
2019-11-29 04:38:37 -0500

    Added a landing page with a logo

---
+++ app/src/main/java/io/github/blist/LandingActivity.java
@@ -0,0 +1,15 @@
+package io.github.blist;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+/* Modified at
+    Nov 29, 2019 */
+public class LandingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.landing);
+    }
+}
